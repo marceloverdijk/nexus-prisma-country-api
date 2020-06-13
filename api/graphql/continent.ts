91fba827fdfd9eda27b95492fb25af32f53cd859
Marcel Overdijk <marceloverdijk@gmail.com>
2020-06-13 21:13:06 +0200

    Initial project setup

---
+++ api/graphql/continent.ts
@@ -0,0 +1,48 @@
+import { schema } from 'nexus'
+
+schema.objectType({
+  name: 'Continent',
+  description: 'Represents a continent.',
+  definition(t) {
+    t.id('id', {
+      description: 'The unique id of the continent.',
+      nullable: false,
+    })
+    t.string('code', {
+      description: 'The unique code of the continent.',
+      nullable: false,
+    })
+    t.string('name', {
+      description: 'The name of the continent.',
+      nullable: false,
+    })
+  },
+})
+
+schema.extendType({
+  type: 'Query',
+  definition(t) {
+    t.field('continent', {
+      type: 'Continent',
+      description: 'Returns the continent.',
+      nullable: true,
+      args: {
+        id: schema.idArg({
+          description: 'The unique id of the continent.',
+          required: true,
+        }),
+      },
+      resolve: (parent, args, ctx) => {
+        return ctx.db.continent.findOne({ where: { id: args.id } })
+      },
+    })
+    t.list.field('continentList', {
+      type: 'Continent',
+      description: 'Returns the continents.',
+      nullable: true,
+      resolve: (parent, args, ctx) => {
+        return ctx.db.continent.findMany()
+      },
+    })
+  },
+})
---
+++ api/graphql/country.ts
@@ -0,0 +1,55 @@
+import { schema } from 'nexus'
+
+schema.objectType({
+  name: 'Country',
+  description: 'Represents a country.',
+  definition(t) {
+    t.id('id', {
+      description: 'The unique id of the country.',
+      nullable: false,
+    })
+    t.string('code', {
+      description: 'The unique ISO 3166-1 alpha-2 code of the country.',
+      nullable: false,
+    })
+    t.string('name', {
+      description: 'The name of the country.',
+      nullable: false,
+    })
+    t.field('continent', {
+      type: 'Continent',
+      description: 'The continent of the country.',
+      resolve: (country, args, ctx) => {
+        return ctx.db.continent.findOne({ where: { id: country.continentId } })
+      },
+    })
+  },
+})
+
+schema.extendType({
+  type: 'Query',
+  definition(t) {
+    t.field('country', {
+      type: 'Country',
+      description: 'Returns the country.',
+      nullable: true,
+      args: {
+        id: schema.idArg({
+          description: 'The unique id of the country.',
+          required: true,
+        }),
+      },
+      resolve: (parent, args, ctx) => {
+        return ctx.db.country.findOne({ where: { id: args.id } })
+      },
+    })
+    t.list.field('countryList', {
+      type: 'Country',
+      description: 'Returns the countries.',
+      nullable: true,
+      resolve: (parent, args, ctx) => {
+        return ctx.db.country.findMany()
+      },
+    })
+  },
+})
